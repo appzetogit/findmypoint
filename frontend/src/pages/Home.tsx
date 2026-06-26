04703703d51eac5b58a978d3a3e8d922423cd742
Ajay Panchal <panchalajay717@gmail.com>
2026-06-26 12:11:19 +0530

    b2

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -814,7 +814,7 @@ export default function HomePage({ onArticleClick, onReviewClick, onPlaceClick, 
 
 
         <section className="mt-8">
-          <div className="grid grid-cols-3 gap-x-4 gap-y-8 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-7 xl:grid-cols-7">
+          <div className="grid grid-cols-3 gap-x-4 gap-y-8 sm:grid-cols-4 md:grid-cols-7 lg:grid-cols-7 xl:grid-cols-7">
             {categories.map(({ img, label }) => (
               <button
                 key={label}
